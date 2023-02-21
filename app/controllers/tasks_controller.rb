b596e9c0e530cb54010352a1b971f54b29ce7f3b
Quentin Doulcet <quentinucme@hotmail.fr>
2023-02-21 16:09:02 -0500

    almost finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,9 +4,29 @@ class TasksController < ApplicationController
    @tasks = Task.all
   end
 
+
+  def new
+    @task = Task.new
+  end
+
   def show
     @task = Task.find(params[:id])
   end
 
-  
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
   get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
+  get 'task/:id/edit', to: 'tasks#edit'
 end
 
 
