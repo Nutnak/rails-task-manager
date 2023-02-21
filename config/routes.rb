09b905629a6e4f5e1a361b8dd356b6553b807004
Quentin Doulcet <quentinucme@hotmail.fr>
2023-02-21 14:15:12 -0500

    on avance

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
+
+
+
+# En tant qu’utilisateur, je peux lister des tâches
+# En tant qu’utilisateur, je peux afficher les détails d’une tâche
+# En tant qu’utilisateur, je peux ajouter une nouvelle tâche
+# En tant qu’utilisateur, je peux éditer une tâche (la marquer comme terminée / mettre à jour le titre et les détails)
+# En tant qu’utilisateur, je peux supprimer une tâche
